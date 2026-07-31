0406cc21602a3a0f9377b65d5ef5042be01a41e8
Tsundoiii <dev@tsundoiii.dev>
2026-07-31 15:35:52 -0400

    more config

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,18 @@
-{ }
+{
+  state.stateVersion = "26.11";
+
+  hardware.enableRedistributableFirmware = true;
+
+  services.openssh.enable = true;
+
+  programs.vim.enable = true;
+
+  users = {
+    mutableUsers = false;
+
+    users.user = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+}
