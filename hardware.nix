69b8c1101b994d922fdd68a5472b2588b39d048c
Tsundoiii <dev@tsundoiii.dev>
2026-08-01 16:00:59 -0400

    fix filesystems

--- hardware.nix
+++ hardware.nix
@@ -3,12 +3,12 @@
   boot.loader.raspberry-pi.bootloader = "kernel";
 
   fileSystems."/" = {
-    device = "44444444-4444-4444-8888-888888888888";
+    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "2178-694E";
+    device = "/dev/disk/by-uuid/2178-694E";
     fsType = "vfat";
   };
 }
